20f9ba8dff66de2e1e1c58f9384019ff3ddc7725
dzamakhaiev <dzamakhaiev@star.global>
2024-07-11 16:27:58 +0300

    update main

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ def main_loop():
 
     for website_name, data in URLS_TO_CHECK.items():
         url = data.get('url')
-        locators_class = data.get('locators')
         page_class = data.get('page')
         driver_class = data.get('driver')
 
         page = page_class(driver=driver_class)
         page.go_to(url)
-        vacancies = page.find_elements(locator=locators_class.VACANCIES)
-        print(len(vacancies))
+        vacancies = page.get_all_vacancies()
 
 
 if __name__ == '__main__':
