d49bf2616709dd62093cdfdb82f5fb62473a2240
dzamakhaiev <dzamakhaiev@star.global>
2024-07-11 16:27:36 +0300

    update dou locators

--- locators.py
+++ locators.py
@@ -8,3 +8,11 @@ class BaseLocators:
 class DouLocators(BaseLocators):
     VACANCIES = (By.CLASS_NAME, 'l-vacancy')
     MORE_BUTTON = (By.CLASS_NAME, 'more-btn')
+    DATE = (By.CLASS_NAME, 'date')
+    TITLE = (By.CLASS_NAME, 'title')
+    COMPANY = (By.TAG_NAME, 'strong')
+    COMPANY_NAME = (By.CLASS_NAME, 'company')
+    URL = (By.CLASS_NAME, 'vt')
+    CITIES = (By.CLASS_NAME, 'cities')
+    INFO = (By.CLASS_NAME, 'sh-info')
+
