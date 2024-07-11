5ac526e1f56d1442edcec3032281770c325ee3aa
dzamakhaiev <dzamakhaiev@star.global>
2024-07-11 18:17:57 +0300

    add main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from urls_to_check import URLS_TO_CHECK
+from sqlite_handler import DatabaseHandler
 
 
 def main_loop():
+
+    db_handler = DatabaseHandler()
 
     for website_name, data in URLS_TO_CHECK.items():
         url = data.get('url')
@@ -11,6 +14,11 @@ def main_loop():
         page = page_class(driver=driver_class)
         page.go_to(url)
         vacancies = page.get_all_vacancies()
+        print(vacancies)
+
+        db_handler.insert_vacancies(vacancies)
+        vacancies_db = db_handler.get_vacancies()
+        print(vacancies_db)
 
 
 if __name__ == '__main__':
---
+++ sqlite_handler.py
@@ -0,0 +1,73 @@
+import sqlite3
+
+
+class DatabaseHandler:
+
+    def __init__(self):
+        try:
+            self.conn = sqlite3.connect('vacancies.db')
+            self.cursor = self.conn.cursor()
+            self.create_vacancies_table()
+        except sqlite3.Error as e:
+            quit(e)
+
+    def cursor_execute(self, query, args=None):
+        try:
+            result = self.cursor.execute(query, args)
+            return result
+
+        except sqlite3.DatabaseError as e:
+            print(e)
+            self.conn.rollback()
+
+    def cursor_with_commit(self, query, args=None, many=False):
+        if args is None:
+            args = []
+
+        try:
+            if many:
+                self.cursor.executemany(query, args)
+            else:
+                self.cursor.execute(query, args)
+            self.conn.commit()
+
+        except sqlite3.DatabaseError as e:
+            print(e)
+            self.conn.rollback()
+
+    def create_vacancies_table(self):
+        self.cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS vacancies
+                    (vacancy_id TEXT NOT NULL UNIQUE,
+                    title TEXT NOT NULL,
+                    company TEXT NOT NULL,
+                    info TEXT NOT NULL,
+                    cities TEXT,
+                    date TEXT,
+                    notified BOOLEAN DEFAULT 0)
+                    ''')
+
+    def get_vacancies(self, limit=10):
+        result = self.cursor_execute('SELECT * FROM vacancies ORDER BY date DESC LIMIT ?', args=(limit,))
+        if result:
+            return result.fetchall()
+
+    def insert_vacancy(self, vacancy_id: str, vacancy_dict: dict):
+        self.cursor_with_commit('INSERT OR IGNORE INTO vacancies (vacancy_id, title, company, info, cities, date)'
+                                ' VALUES (?, ?, ?, ?, ?, ?)',
+                                (vacancy_id, vacancy_dict.get('title'), vacancy_dict.get('company'),
+                                 vacancy_dict.get('info'), vacancy_dict.get('cities'), vacancy_dict.get('date')))
+
+    def insert_vacancies(self, vacancies: dict):
+        args = []
+        for vacancy_id, vacancy_dict in vacancies.items():
+            args.append((vacancy_id, vacancy_dict.get('title'), vacancy_dict.get('company'),
+                         vacancy_dict.get('info'), vacancy_dict.get('cities'), vacancy_dict.get('date')))
+
+        self.cursor_with_commit('INSERT OR IGNORE INTO vacancies (vacancy_id, title, company, info, cities, date)'
+                                ' VALUES (?, ?, ?, ?, ?, ?)', args=args, many=True)
+
+    def __del__(self):
+        if self.conn and self.cursor:
+            self.cursor.close()
+            self.conn.close()
