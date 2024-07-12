6217a906bf73ef21459a7d7581abdbb96b7630b4
dzamakhaiev <dzamakhaiev@star.global>
2024-07-12 11:55:02 +0300

    add filter for vacancies

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from urls_to_check import URLS_TO_CHECK
 from sqlite_handler import DatabaseHandler
 
 
-def main_loop():
+db_handler = DatabaseHandler()
 
-    db_handler = DatabaseHandler()
+
+def filter_vacancies(vacancies: dict):
+    vacancies_db = db_handler.get_vacancies()
+    for vacancy in vacancies_db:
+        vacancy_id = vacancy[0]
+
+        if vacancy_id in vacancies:
+            vacancies.pop(vacancy_id)
+
+
+def main_loop():
 
     for website_name, data in URLS_TO_CHECK.items():
         url = data.get('url')
@@ -14,6 +24,9 @@ def main_loop():
         page = page_class(driver=driver_class)
         page.go_to(url)
         vacancies = page.get_all_vacancies()
+        print(vacancies)
+
+        filter_vacancies(vacancies)
         print(vacancies)
 
         db_handler.insert_vacancies(vacancies)
