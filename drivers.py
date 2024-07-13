1ce25403bf1b01018abf8d75fa9e13c9e3fd3b61
dzamakhaiev <dzamakhaiev@star.global>
2024-07-13 15:10:26 +0300

    add binary driver

--- drivers.py
+++ drivers.py
@@ -1,6 +1,5 @@
 import platform
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from latest_user_agents import get_random_user_agent
 from logger import Logger
 
@@ -37,9 +36,7 @@ class ChromeDriver(BaseDriver):
     def __init__(self):
         logger.info(f'Platform: {platform.system()}: {platform.release()}')
         if platform.system().startswith('Linux'):
-            manager = ChromeDriverManager()
-            driver_path = manager.install()
-            service = webdriver.ChromeService(executable_path=driver_path)
+            service = webdriver.ChromeService(executable_path='drivers/chromedriver')
         else:
             service = webdriver.ChromeService()
 
