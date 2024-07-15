4210709c073c240668a20327f8f72401e38e88e3
dzamakhaiev <dzamakhaiev@star.global>
2024-07-15 12:36:32 +0300

    add globallogic

--- urls_to_check.py
+++ urls_to_check.py
@@ -1,11 +1,13 @@
 import pages
 import drivers
-import locators
 
 
 URLS_TO_CHECK = {
     'dou': {'url': 'https://jobs.dou.ua/vacancies/?category=QA&search=automation+python&descr=1',
-            'locators': locators.DouLocators, 'page': pages.DouVacanciesPage, 'driver': drivers.ChromeDriver},
+            'page': pages.DouVacanciesPage, 'driver': drivers.ChromeDriver},
     'luxoft': {'url': 'https://career.luxoft.com/jobs?keyword=qa%20automation&country[]=Ukraine&perPage=60',
-               'locators': locators.LuxoftLocators, 'page': pages.LuxoftVacanciesPage, 'driver': drivers.ChromeDriver}
+               'page': pages.LuxoftVacanciesPage, 'driver': drivers.ChromeDriver},
+    'globallogic': {'url': 'https://www.globallogic.com/ua/career-search-page/'
+                           '?keywords=qa+automation&experience=&locations=ukraine&c=&workmodel=Remote',
+                    'page': pages.GlobalLogicVacanciesPage, 'driver': drivers.ChromeDriver}
 }
